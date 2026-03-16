7f546875435f8da0f0c03e0c5495312d22219865
AryanMore4335 <aryanmore433518@gmail.com>
2026-03-16 10:50:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import telebot
 import yt_dlp
 import os
-from telebot import types
 
 TOKEN = "7757411907:AAGaYXArUQbz6qC14SUd12kLe05MW9oYGY0"
+CHANNEL = "@Aryan_vaishu"
 ADMIN_ID = 7757026734
-CHANNEL = "@Aryan_Vaishu"
 
 bot = telebot.TeleBot(TOKEN)
 
 users = set()
 
-def check_join(user_id):
+
+def is_joined(user_id):
     try:
         member = bot.get_chat_member(CHANNEL, user_id)
         return member.status in ["member","administrator","creator"]
@@ -22,54 +22,52 @@ def check_join(user_id):
 @bot.message_handler(commands=['start'])
 def start(message):
 
-    if not check_join(message.chat.id):
+    user_id = message.from_user.id
+    users.add(user_id)
 
-        markup = types.InlineKeyboardMarkup()
-        button = types.InlineKeyboardButton(
+    if not is_joined(user_id):
+
+        markup = telebot.types.InlineKeyboardMarkup()
+
+        join_btn = telebot.types.InlineKeyboardButton(
             "Join Channel",
             url=f"https://t.me/{CHANNEL.replace('@','')}"
         )
 
-        markup.add(button)
+        markup.add(join_btn)
 
         bot.send_message(
             message.chat.id,
-            "⚠️ Join our channel to use the bot",
+            "Welcome to Aryan Insta Download Bot\n\nFor use bot join this channel",
             reply_markup=markup
         )
         return
 
-    users.add(message.chat.id)
-
-    bot.send_message(
-        message.chat.id,
-        "Send Instagram Reel Link"
-    )
+    bot.send_message(message.chat.id,"Send Instagram Reel Link")
 
 
 @bot.message_handler(commands=['stats'])
 def stats(message):
 
-    if message.chat.id == ADMIN_ID:
-        bot.send_message(
-            message.chat.id,
-            f"👥 Total Users: {len(users)}"
-        )
+    if message.from_user.id == ADMIN_ID:
+        bot.send_message(message.chat.id,f"Total Users: {len(users)}")
 
 
 @bot.message_handler(commands=['broadcast'])
 def broadcast(message):
 
-    if message.chat.id != ADMIN_ID:
+    if message.from_user.id != ADMIN_ID:
         return
 
-    text = message.text.replace("/broadcast ", "")
+    text = message.text.replace("/broadcast ","")
 
     for user in users:
         try:
-            bot.send_message(user, text)
+            bot.send_message(user,text)
         except:
             pass
+
+    bot.send_message(message.chat.id,"Broadcast Sent")
 
 
 @bot.message_handler(func=lambda m: "instagram.com" in m.text)
